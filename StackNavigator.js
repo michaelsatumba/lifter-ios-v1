77ca54d51f6e95a0bb9095e382298ac0d2ed1c23
michaelsatumba <mjsatumba@gmail.com>
2021-12-03 16:14:34 -0800

    initial

---
+++ StackNavigator.js
@@ -0,0 +1,27 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import ChatScreen from './screens/ChatScreen';
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigator = () => {
+    const user = false;
+    return (
+        <Stack.Navigator>
+        {user ? (
+            <>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Chat" component={ChatScreen} />
+            </>
+        ) : (
+            <Stack.Screen name="Login" component={LoginScreen} />
+        )}
+            
+            
+        </Stack.Navigator>
+    )
+}
+
+export default StackNavigator
---
+++ screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>Log in to the app</Text>
+        </View>
+    )
+}
+
+export default LoginScreen
